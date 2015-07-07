489db0c4aa1f4cc7fce46f4a7e5a65cac2b1a9b7
Skewalramani <sagar.kewalramani@gmail.com>
2015-07-07 18:49:00 +0530

    Update 2

--- copy.sh
+++ copy.sh
@@ -8,34 +8,34 @@ echo "Running for Module "$Module
 PRD=ec2-52-6-180-136.compute-1.amazonaws.com
 QA2=ec2-52-6-180-136.compute-1.amazonaws.com
 INIT=ec2-52-6-180-136.compute-1.amazonaws.com
-if [ "$Environment" == "prd" ] && [ "$Module" == "classification" ]; then
+if [ "$Environment" == "PRD" ] && [ "$Module" == "Classification" ]; then
 	appServer=$PRD
 	target=/opt/node-v0.12.5-linux-x64/bin/classification/
 	source1=/var/lib/jenkins/jobs/Project2/workspace/PRD/Classification/DP_classification_v1.js
 	source2=/var/lib/jenkins/jobs/Project2/workspace/PRD/Classification/DP_classification_v1.properties
-elif [ "$Environment" == "prd" ] && [ "$Module" == "extraction" ]; then
+elif [ "$Environment" == "PRD" ] && [ "$Module" == "Extraction" ]; then
 	appServer=$PRD
 	target=/opt/node-v0.12.5-linux-x64/bin/extraction/
 	source1=/var/lib/jenkins/jobs/Project2/workspace/PRD/Extraction/DP_Extraction_V1.js
 	source2=/var/lib/jenkins/jobs/Project2/workspace/PRD/Extraction/DP_Extraction.properties
 fi
-if [ "$Environment" == "QA2" ] && [ "$Module" == "classification" ]; then
+if [ "$Environment" == "QA2" ] && [ "$Module" == "Classification" ]; then
 	appServer=$QA2
 	target=/opt/node-v0.12.5-linux-x64/bin/classification/
 	source1=/var/lib/jenkins/jobs/Project2/workspace/QA2/Classification/DP_classification_v1.js
 	source2=/var/lib/jenkins/jobs/Project2/workspace/QA2/Classification/DP_classification_v1.properties
-elif [ "$Environment" == "QA2" ] && [ "$Module" == "extraction" ]; then
+elif [ "$Environment" == "QA2" ] && [ "$Module" == "Extraction" ]; then
 	appServer=$QA2
 	target=/opt/node-v0.12.5-linux-x64/bin/extraction/
 	source1=/var/lib/jenkins/jobs/Project2/workspace/QA2/Extraction/DP_Extraction_V1.js
 	source2=/var/lib/jenkins/jobs/Project2/workspace/QA2/Extraction/DP_Extraction.properties
 fi
-if [ "$Environment" == "INIT" ] && [ "$Module" == "classification" ]; then
+if [ "$Environment" == "INIT" ] && [ "$Module" == "Classification" ]; then
 	appServer=$INIT
 	target=/opt/node-v0.12.5-linux-x64/bin/classification/
 	source1=/var/lib/jenkins/jobs/Project2/workspace/INIT/Classification/DP_classification_v1.js
 	source2=/var/lib/jenkins/jobs/Project2/workspace/INIT/Classification/DP_classification_v1.properties
-elif [ "$Environment" == "INIT" ] && [ "$Module" == "extraction" ]; then
+elif [ "$Environment" == "INIT" ] && [ "$Module" == "Extraction" ]; then
 	appServer=$INIT
 	target=/opt/node-v0.12.5-linux-x64/bin/extraction/
 	source1=/var/lib/jenkins/jobs/Project2/workspace/INIT/Extraction/DP_Extraction_V1.js
